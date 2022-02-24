6ac7fe68d487832f7f20de5eae5bf7d897241cb3
Ioana <ioanasong@gmail.com>
2022-02-24 21:48:24 +0100

    ignore

---
+++ projectsTest.py
@@ -0,0 +1,10 @@
+
+
+class projects:
+    def __init__(self, no_people, programming_languages, best_before, no_of_rules):
+        """Create a new project with the required information """
+
+        self.no_people = no_people
+        self.programming_languages = programming_languages
+
+    def sort_projects_data():
--- test2.py
+++ test2.py
@@ -6,15 +6,24 @@ info = open("a_an_example.in.txt").read().split("\n")
 first_line = info[0].split(" ")
 i = 0
 people = 0
+projects = 0
 while people < int(first_line[0]):
+    nr_skills = 0
     print(info[i + 1])
     contributor = info[i+1].split(" ")
     # individual contributor class goes here
+    skills = []
     for j in range(0, int(contributor[1])):
-        skills.append(info[i+j+1])
+        skills.append(info[i+j+2])
+        for skill in skills:
+            list_skills = skill.split(" ")
+            print(list_skills[0])  # add here class for skill
+            print(list_skills[1])  # add here class for level
         nr_skills += 1
         # skills class
     i += nr_skills + 1
     print(skills)
     people += 1
+# while projects < int(first_line[1]):
+#     print(info[i+1]
 
