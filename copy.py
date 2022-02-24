9f0440ea9928a8011db18e208be5a9956d5a21d7
Ioana <ioanasong@gmail.com>
2022-02-24 20:58:00 +0100

    nothing

---
+++ copy.py
@@ -0,0 +1,21 @@
+import sys
+skills = []
+nr_skills = 0
+
+info = sys.stdin.read().split("\n")
+first_line = info[0].split(" ")
+i = 0
+people = 0
+while people < int(first_line[0]):
+    print(info[i + 1])
+    contributor = info[i+1].split(" ")
+    # individual contributor class goes here
+    for j in range(0, int(contributor[1])):
+        skills.append(info[i+j+2])
+        nr_skills += 1
+        # skills class
+    i += nr_skills + 1
+    print(skills)
+    people += 1
+
+#while 
